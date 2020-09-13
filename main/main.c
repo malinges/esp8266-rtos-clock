631bea3131224f192a55a55434cf8e11cc921160
Simon Malinge <simon.malinge@gmail.com>
2020-09-13 19:06:49 -0400

    remove LwIP example header

--- main/main.c
+++ main/main.c
@@ -1,11 +1,3 @@
-/* LwIP SNTP example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
